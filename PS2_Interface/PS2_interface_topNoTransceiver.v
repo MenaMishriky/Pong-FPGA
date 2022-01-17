913fa130fd9341f5d6179932c395bc5087ade09d
Taniya Shaffer <shaffertaniya@gmail.com>
2022-01-17 16:11:08 -0500

    Introduced state machines into transmitter

--- PS2_Interface/PS2_interface_topNoTransceiver.v
+++ PS2_Interface/PS2_interface_topNoTransceiver.v
@@ -8,32 +8,95 @@
 // 5. Convert bits into paddle direction and paddle speed
 `timescale 1ns / 1ps
 module mouse_ps2_verilog( input wire clk_25MHz,
-                       input wire ps2_clk,
-                       input wire data_in,
-                       input wire reset,
-                       output reg paddle_dir,
-                       output reg [7:0] paddle_speed,
-                       output reg error_flag,
-                       output reg new_output_flag);
+                        input wire ps2_clk,
+                        input wire data_in,
+                        input wire reset,
+                        output reg paddle_dir,
+                        output reg [7:0] paddle_speed,
+                        output reg error_flag,
+                        output reg new_output_flag);
         reg [32:0] ps2_data;                                                    // big register to hold 33-bit mouse data
         reg [5:0] bit_counter;                                                  // 0-33 counter to make sure all bits are present
         reg new_output_history;                                                 // flag history to validate that new output is indeed present
         
-        reg [10:0] special_command;                                             //holds special commands (eg: enable reporting)
+        reg [10:0] special_command_in;                                          //holds special commands received (eg: acknowledge)
+        reg [10:0] special_command_out;                                         //holds special commands to be sent (eg: enable data reporting)
         reg [3:0] special_counter;                                              //counter for special commands
-        
-        
-        // Always block that controls transmitting to the mouse
+        reg [1:0] state;
+        reg [1:0] next_state;                                                     
+        wire ID_received, Ack_received;                                         //Ack_received is after Enable reporting
+        reg data_out;
 
+        localparam READ_SP = 2'b00;
+        localparam WRITE_SP = 2'b01;
+        localparam READ_DATA = 2'b11;
+        localparam DATA_EN_REPORT = 8'hF4;
+        localparam ACK = 8'hFA;
+        localparam ID = 8'h00;
+        
+        // Always block containing state machine that controls how states are changed
+        always @(reset, ID_received, Ack_received, state) begin
+            if (reset) begin
+                next_state <= READ_SP;
+                special_command_out <= 11'b00000000010;
+            end
+            else begin
+                case (state)
+                    READ_SP       :  begin                                         // case 1: BAT passed, streaming mode enabled, do nothing
+                                        next_state = ID_received ? WRITE_SP : READ_SP;
+                                    end
+                    WRITE_SP      : begin                                          // case 2: mouse_ID received, write data enable reporting
+                                        next_state = Ack_received ? READ_DATA : READ_SP; 
+                                    end
+                    READ_DATA     : begin                                          // case 3: mouse ack received, read ps2_data and ps2_clk, do nothing
+                                        next_state = READ_DATA;
+                                    end                                                                 
+                endcase
+            end
+                 
+        end
 
+        // Always block containing state machine that controls when states are changed
+        always @(posedge reset or posedge ps2_clk or negedge ps2_clk) begin
+            if (reset) begin
+                state <= READ_SP;
+                special_command_in <= 11'b00000000010;
+                special_command_out <= {1'b1,1'b1,DATA_EN_REPORT,1'b0}; //stop, parity, data, start
+                special_counter <= 0;
+            end
+            else begin
+                case (state)
+                    READ_SP       :  begin                                         // case 1: BAT passed, streaming mode enabled, do nothing
+                                        if(!ps2_clk) begin
+                                            
+                                        end
+                                    end
+                    WRITE_SP      : begin                                          // case 2: on posedge ps2_clk, shift special_command_out to data_in
+                                        if(ps2_clk) begin                          
+                                            data_out <= special_command_out[0];     
+                                            special_command_out[9:0] <= special_command_out[10:1];
+                                            if (special_counter < 11) begin
+                                                special_counter <= special_counter + 1;
+                                            end
+                                            else
+                                                special_counter <= 1;
+                                        end
+                                    end
+                    READ_DATA     : begin                                          // case 3: mouse ack received, read ps2_data and ps2_clk, do nothing
+                                        
+                                    end                                                                 
+                endcase
+            end    
+        end
+
+        // Always block for valid bit checker : check if the start and/or stop bits are correct for each word in MSB
         always @(posedge ps2_clk or posedge reset) begin
-            // Conduct valid checker : check if the start and/or stop bits are correct for each word in MSB
             if (reset) begin
                 error_flag <= 0;
                 paddle_dir <= 0;
                 paddle_speed <= 0;
             end
-            else if (ps2_data[32] == 0) begin                                        // stop bit, third word
+            else if (ps2_data[32] == 0) begin                                   // stop bit, third word
                 error_flag <= 1;
             end
             else if (ps2_data[22] == 1)                                         // start bit, third word
@@ -54,6 +117,7 @@ module mouse_ps2_verilog( input wire clk_25MHz,
                 error_flag <= 0;                
         end
         
+        // Always block responsible for shifting the mouse data into the register
         always @(negedge ps2_clk or posedge reset) begin
             // Initialize
             if (reset) begin
@@ -72,7 +136,8 @@ module mouse_ps2_verilog( input wire clk_25MHz,
                     bit_counter <= 1;                                               // received all bits
             end
         end
-         
+
+        // Always block responsible for checking new output is valid 
         always @(posedge clk_25MHz or posedge reset) begin
             if (reset) begin
                new_output_flag <= 0;
